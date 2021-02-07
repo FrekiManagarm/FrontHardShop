6a939438be8b176668b7e4ae3451bbfef621c9f0
FrekiManagarm <mathieu.chambaud@ynov.com>
2021-02-07 19:38:47 +0100

    Front and Back on two different repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+const App = () => {
+
+  return (
+    <div className="box-border m-0 font-sans p-0">
+       <Navbar />   
+    </div>
+  )
+}
+
+
+export default App;
