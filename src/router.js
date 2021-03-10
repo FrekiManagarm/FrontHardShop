6d72840f2ec464269e727d8935133d368f94f122
Freki Managarm <mathieu.chambaud@ynov.com>
2021-03-10 23:59:55 +0100

    last commit of this day

--- src/router.js
+++ src/router.js
@@ -7,18 +7,32 @@ import PSUList from './pages/ComponentsList/PSUList/PSUList';
 import RAMList from './pages/ComponentsList/RAMList/RAMList';
 import SSDList from './pages/ComponentsList/SSDList/SSDList';
 import HomePage from './pages/HomePage';
+import Step1 from './pages/Configurator/Step1/Step1';
+import Step2 from './pages/Configurator/Step2/Step2';
+import Step3 from './pages/Configurator/Step3/Step3';
+import Step4 from './pages/Configurator/Step4/Step4';
+import Step5 from './pages/Configurator/Step5/Step5';
+import Step6 from './pages/Configurator/Step6/Step6';
+import Step7 from './pages/Configurator/Step7/Step7';
 
 export const AppRouter = () => {
     return (
         <Switch>
             <Route component={HomePage} exact path="/" />
-            <Route component={CPUList} path="" />
-            <Route component={GPUList} path="" />
-            <Route component={HDDList} path="" />
-            <Route component={MotherBoardList} path="" />
-            <Route component={PSUList} path="" />
-            <Route component={RAMList} path="" />
-            <Route component={SSDList} path="" />
+            <Route component={CPUList} path="/CPUs" />
+            <Route component={GPUList} path="/GPUs" />
+            <Route component={HDDList} path="/HDDs" />
+            <Route component={MotherBoardList} path="/MotherBoards" />
+            <Route component={PSUList} path="/PSUs" />
+            <Route component={RAMList} path="/RAMs" />
+            <Route component={SSDList} path="/SSDs" />
+            <Route component={Step1} path="/Configurator/Step1"  />
+            <Route component={Step2} path="/Configurator/Step2"  />
+            <Route component={Step3} path="/Configurator/Step3"  />
+            <Route component={Step4} path="/Configurator/Step4"  />
+            <Route component={Step5} path="/Configurator/Step5"  />
+            <Route component={Step6} path="/Configurator/Step6"  />
+            <Route component={Step7} path="/Configuration/Step7"  />
         </Switch>
     )
 }
