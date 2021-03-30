8d3216b1fa503b60a35b3b89cd45e4a441df2366
FrekiManagarm <mathieu.chambaud@ynov.com>
2021-03-30 21:08:56 +0200

    following login and register form

--- src/pages/ComponentsList/GPUList/GPUList.jsx
+++ src/pages/ComponentsList/GPUList/GPUList.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
 import { GPUListPageWrapper } from './GPUList.style'
 
 const GPUList = () => {
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const { data } = await axios.get('');
+        setData(data);
+    })
+
     return (
         <GPUListPageWrapper>
-            
+            <pre>{JSON.stringify(data)}</pre>
         </GPUListPageWrapper>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router'
 import { LoginPageWrapper } from './Login.style'
 
 const Login = () => {
+
+
+    const history = useHistory();
+    const [login, setLogin] = useState({
+        email: '',
+        password: '',
+        errors: {}
+    })
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        history.push('/');
+    }
+
+    const handleInput = (e) => {
+        e.preventDefault();
+        const name = e.target.name;
+        const value = e.target.value;
+        setLogin({[name]: value})
+    }
+
     return (
         <LoginPageWrapper>
             <div className="other">
                 <div className="another">
-                    <form className="the-form">
-                        <label>Email</label>
-                        <input type="email" name="email" placeholder="Renseignez votre email" className="first-input" />
-                        <label>Mot de passe</label>
-                        <input type="password" name="password" placeholder="Renseignez votre mot de passe" className="first-input" 
-                         />
+                    <form className="the-form" onSubmit={handleForm}> 
+                        <div>
+                            <label>Email</label>
+                            <input type="email" name="email" placeholder="Renseignez votre email" className="first-input" onChange={handleInput} />
+                        </div>
+                        <div style={{ marginTop: "1rem" }}>
+                            <label>Mot de passe</label>
+                            <input type="password" name="password" placeholder="Renseignez votre mot de passe" className="first-input" onChange={handleInput} />
+                        </div>
+
                     </form>
                 </div>
             </div>
